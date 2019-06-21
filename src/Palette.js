86419ce8903c5b902cebb24f469e25ace146e928
Viet Nguyen <vmnguyen@ryerson.ca>
2019-06-21 19:36:50 -0400

    seedColors.js

---
+++ src/Palette.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export default class Palette extends Component {
+  render() {
+    return (
+      <div className="Palette">
+        {/* NavBar goes here */}
+        <div className="Palette-colors">
+          <div />
+        </div>
+        {/* Footer later */}
+        <h1>Palette</h1>
+      </div>
+    );
+  }
+}
