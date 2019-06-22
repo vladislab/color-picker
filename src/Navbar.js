3babbe37c2cbd48d40a2ad75c6b82a8901b2751c
Viet Nguyen <vmnguyen@ryerson.ca>
2019-06-22 17:59:53 -0400

    Added color format select

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
 import Slider, { Range } from "rc-slider";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 import "rc-slider/assets/index.css";
 import "./Navbar.css";
 
 export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { format: "hex" };
+  }
+
+  handleChange = e => {
+    this.setState({ format: e.target.value }, () =>
+      this.props.handleChange(this.state.format)
+    );
+  };
   render() {
     const { level, changeLevel } = this.props;
+    const { format } = this.state;
     return (
       <header className="Navbar">
         <div className="logo">
@@ -22,6 +35,13 @@ export default class Navbar extends Component {
               onAfterChange={changeLevel}
             />
           </div>
+        </div>
+        <div className="select-container">
+          <Select value={format} onChange={this.handleChange}>
+            <MenuItem value="hex">HEX - #ffffff</MenuItem>
+            <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+            <MenuItem value="rgba">RGBA - rgba(255,255,255,1.0)</MenuItem>
+          </Select>
         </div>
       </header>
     );
--- src/Palette.js
+++ src/Palette.js
@@ -6,20 +6,27 @@ import "./Palette.css";
 export default class Palette extends Component {
   constructor(props) {
     super(props);
-    this.state = { level: 500 };
+    this.state = { level: 500, format: "hex" };
   }
   changeLevel = level => {
     this.setState({ level: level });
   };
+  changeFormat = val => {
+    this.setState({ format: val });
+  };
   render() {
     const { colors } = this.props.palette;
-    const { level } = this.state;
+    const { level, format } = this.state;
     const colorBoxes = colors[level].map(color => (
-      <ColorBox background={color.hex} name={color.name} />
+      <ColorBox background={color[format]} name={color.name} />
     ));
     return (
       <div className="Palette">
-        <Navbar level={level} changeLevel={this.changeLevel} />
+        <Navbar
+          level={level}
+          changeLevel={this.changeLevel}
+          handleChange={this.changeFormat}
+        />
         {/* NavBar goes here */}
         <div className="Palette-colors">{colorBoxes}</div>
         {/* Footer later */}
