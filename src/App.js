ee10048cb553f59a786a12673adad3984c66376a
Viet Nguyen <vmnguyen@ryerson.ca>
2019-06-21 21:29:59 -0400

    different shades generator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      {console.log(generatePalette(seedColors[4]))}
       <Palette {...seedColors[4]} />
     </div>
   );
