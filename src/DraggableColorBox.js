823cbc41d93e8211e17389be10d1dcff1d917dd5
Viet Nguyen <vmnguyen@ryerson.ca>
2019-06-25 16:53:01 -0400

    refactor all styles into separate files

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -2,38 +2,8 @@ import React from "react";
 import { withStyles } from "@material-ui/styles";
 import { SortableElement } from "react-sortable-hoc";
 import DeleteForeverRoundedIcon from "@material-ui/icons/DeleteForeverRounded";
+import styles from "./style/DraggableColorBoxStyle";
 
-const styles = {
-  root: {
-    width: "20%",
-    height: "25%",
-    margin: "0 auto",
-    display: "inline-block",
-    position: "relative",
-    cursor: "pointer",
-    marginBottom: "-6px",
-    "&:hover svg": {
-      color: "white",
-      transform: "scale(1.5)"
-    }
-  },
-  boxContent: {
-    position: "absolute",
-    padding: "10px",
-    width: "100%",
-    left: "0px",
-    bottom: "0px",
-    color: "rgba(0,0,0,0.5)",
-    letterSpacing: "1px",
-    textTransform: "uppercase",
-    fontSize: "12px",
-    display: "flex",
-    justifyContent: "space-between"
-  },
-  deleteIcon: {
-    transition: "all 0.1s ease-in-out"
-  }
-};
 const DraggableColorBox = SortableElement(props => {
   return (
     <div
--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -12,66 +12,7 @@ import DraggableColorList from "./DraggableColorList";
 import { arrayMove } from "react-sortable-hoc";
 import PaletteFormNav from "./PaletteFormNav";
 import ColorPickerForm from "./ColorPickerForm";
-
-const drawerWidth = 400;
-
-const useStyles = theme => ({
-  root: {
-    display: "flex"
-  },
-
-  hide: {
-    display: "none"
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0
-  },
-  drawerPaper: {
-    width: drawerWidth,
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  drawerHeader: {
-    display: "flex",
-    alignItems: "center",
-    padding: "0 8px",
-    ...theme.mixins.toolbar,
-    justifyContent: "flex-end"
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(0),
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    }),
-    marginLeft: -drawerWidth,
-    height: "calc(100vh - 64px)"
-  },
-  contentShift: {
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    }),
-    marginLeft: 0
-  },
-  container: {
-    width: "90%",
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  buttons: {
-    width: "100%"
-  },
-  button: {
-    width: "50%"
-  }
-});
+import styles from "./style/NewPaletteFormStyle";
 
 class NewPaletteForm extends Component {
   static defaultProps = {
@@ -212,4 +153,4 @@ class NewPaletteForm extends Component {
     );
   }
 }
-export default withStyles(useStyles, { withTheme: true })(NewPaletteForm);
+export default withStyles(styles, { withTheme: true })(NewPaletteForm);
