ccc6bb9f26ebc13851ce32e6fe86a7e32db3d20d
Viet Nguyen <vmnguyen@ryerson.ca>
2019-06-26 15:38:49 -0400

    Updates

---
+++ precache-manifest.7b91d0806e21e2919b3fe471fdf518d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eb1d030722cf1a8231df2b1a7bedd79",
+    "url": "/color-picker/index.html"
+  },
+  {
+    "revision": "fee5a9b62aad038e67f9",
+    "url": "/color-picker/static/css/2.e8e75e1a.chunk.css"
+  },
+  {
+    "revision": "d9fc321535fffe1a7e6b",
+    "url": "/color-picker/static/css/main.5ceb78df.chunk.css"
+  },
+  {
+    "revision": "fee5a9b62aad038e67f9",
+    "url": "/color-picker/static/js/2.254e9681.chunk.js"
+  },
+  {
+    "revision": "d9fc321535fffe1a7e6b",
+    "url": "/color-picker/static/js/main.5342c61e.chunk.js"
+  },
+  {
+    "revision": "a7d8e61640eb3f8830d1",
+    "url": "/color-picker/static/js/runtime~main.3187d74d.js"
+  },
+  {
+    "revision": "d69a006f148cedf1c0751047842611d6",
+    "url": "/color-picker/static/media/bg1.d69a006f.svg"
+  }
+]);
